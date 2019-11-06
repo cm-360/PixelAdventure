291e65dd88c965d6089027c59e2751d2cfabb53b
cm-360 <cm360channel@gmail.com>
2019-11-05 20:53:47 -0500

    Added comparison methods to KeyCombo

--- src/lib/io/user/KeyCombo.java
+++ src/lib/io/user/KeyCombo.java
@@ -7,17 +7,28 @@ public class KeyCombo {
 	private int mouseButton;
 	private ArrayList<Integer> keys;
 	
+	// Constructor
 	public KeyCombo(int mouseButton, ArrayList<Integer> keyValues) {
 		this.mouseButton = mouseButton;
 		this.keys = keyValues;
 	}
 	
+	// Access methods
 	public int getMouse() {
 		return mouseButton;
 	}
 	
 	public ArrayList<Integer> getKeys() {
 		return keys;
+	}
+	
+	// Comparison methods
+	public boolean equals(KeyCombo otherCombo) {
+		return mouseButton == otherCombo.mouseButton && keys.equals(otherCombo.keys);
+	}
+	
+	public boolean containsAll(KeyCombo otherCombo) {
+		return mouseButton == otherCombo.mouseButton && keys.containsAll(otherCombo.keys);
 	}
 
 }
