22304f7be3a2385d332bd6c91859c25e7006e531
cm-360 <cm360channel@gmail.com>
2019-11-05 21:54:34 -0500

    Added paint method and fixed backwards assertion

--- src/modules/pixadv/layouts/components/generic/Button.java
+++ src/modules/pixadv/layouts/components/generic/Button.java
@@ -1,11 +1,14 @@
 package modules.pixadv.layouts.components.generic;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.util.HashMap;
 
 import lib.io.user.KeyCombo;
 import pixadv.graphics.layouts.components.LayoutComponent;
+import pixadv.registry.Registry;
 
 public class Button extends LayoutComponent {
 
@@ -22,7 +25,7 @@ public class Button extends LayoutComponent {
 		if (LayoutComponent.makeScreenCoords(gBounds, getBounds(variables)).contains(p)) {
 			boolean handled = false;
 			for (KeyCombo combo : clickEvents.keySet())
-				if (combo.containsAll(keys)) {
+				if (keys.containsAll(combo)) {
 					handled = true;
 					clickEvents.get(combo).run();
 				}
@@ -34,6 +37,15 @@ public class Button extends LayoutComponent {
 	
 	public boolean processHover(Rectangle gBounds, Point p, KeyCombo keys, HashMap<String, Double> variables) {
 		return true;
+	}
+	
+	// Paint method
+	public void paint(Graphics g, Registry registry, HashMap<String, Double> variables) {
+		Rectangle selfBounds = makeScreenCoords(g.getClipBounds(), getBounds(variables));
+		g.setColor(Color.WHITE);
+		g.fillRect(selfBounds.x, selfBounds.y, selfBounds.width, selfBounds.height);
+		g.setColor(Color.MAGENTA);
+		g.drawRect(selfBounds.x, selfBounds.y, selfBounds.width, selfBounds.height);
 	}
 
 }
