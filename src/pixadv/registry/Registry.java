95af433bec1f98cd8954f775dfd000e6fcb85b1c
cm-360 <cm360channel@gmail.com>
2019-11-08 18:00:41 -0500

    Registry now works when inside a compiled JAR

--- src/pixadv/registry/Registry.java
+++ src/pixadv/registry/Registry.java
@@ -1,14 +1,18 @@
 package pixadv.registry;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileFilter;
-import java.net.MalformedURLException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import javax.swing.ImageIcon;
 
@@ -32,17 +36,16 @@ public class Registry {
 	// Constructors
 	@SuppressWarnings("unchecked")
 	public Registry() {
+		// Create empty image for air texture
+		TEXTURES.put("pixadv/tile/air", new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB));
 		// Get URL to load builtin classes
-		String path = getClass().getResource(getClass().getSimpleName() + ".class").getFile();
-		URL codePath = null;
-		if (path.startsWith("/")) {
-			// Loading from IDE
-			codePath = getClass().getProtectionDomain().getCodeSource().getLocation();
-			try {
-				// Load resources from directory
-				File codePathFile = new File(codePath.toURI());
+		try {
+			String path = getClass().getResource(getClass().getSimpleName() + ".class").getFile();
+			URL codePath = getClass().getProtectionDomain().getCodeSource().getLocation();
+			File codePathFile = new File(codePath.toURI());
+			if (path.startsWith("/")) {
+				// Loading from IDE directory
 				System.out.printf("Searching directory: '%s'\n", codePathFile);
-				TEXTURES.put("pixadv/tile/air", new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB));
 				// Recursive list of all files in directory
 				System.out.println("Loading resources from <BUILTIN>...");
 				ArrayList<File> files = FileUtil.listFiles(codePathFile, new FileFilter() {
@@ -70,38 +73,70 @@ public class Registry {
 						}
 					}
 				}
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
+			} else {
+				try {
+					// Loading from inside compiled JAR
+					System.out.printf("Searching JAR: '%s'\n", codePathFile);
+					// Recursive list of all files in JAR
+					System.out.println("Loading resources from <BUILTIN>...");
+					ZipFile codePathZip = new ZipFile(codePathFile);
+					Enumeration<? extends ZipEntry> files = codePathZip.entries();
+					while (files.hasMoreElements()) {
+						ZipEntry entry = files.nextElement();
+						if (!entry.isDirectory()) {
+							String relativeName = entry.getName().replaceAll("^modules\\/", "");
+							if (relativeName.endsWith(".png")) {
+								try {
+									String textureID = relativeName.replace("textures/", "").replaceAll("\\.png$", "");
+									// Load all bytes of zip entry
+									InputStream imageStream = codePathZip.getInputStream(entry);
+									ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+									int count;
+									byte[] data = new byte[4096];
+									while ((count = imageStream.read(data, 0, data.length)) != -1) {
+										buffer.write(data, 0, count);
+									}
+									imageStream.close();
+									// Load image and keep transparency
+									ImageIcon iconImage = new ImageIcon(buffer.toByteArray());
+									BufferedImage bufferedImage = new BufferedImage(iconImage.getIconWidth(), iconImage.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+									bufferedImage.getGraphics().drawImage(iconImage.getImage(), 0, 0, null);
+									TEXTURES.put(textureID, bufferedImage);
+									System.out.printf("  Loaded texture '%s' (modules/%s)\n", textureID, relativeName);
+								} catch (Exception e) {
+									System.out.printf("  Error loading texture: 'modules/%s'\n", relativeName);
+									System.out.printf("  %s: %s\n", e.getClass().getName(), e.getMessage());
+								}
+							}
+						}
+					}
+					// Close ZipFile of JAR
+					codePathZip.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-		} else {
+			// Load object classes of builtin module
 			try {
-				// Loading from JAR
-				path = ClassLoader.getSystemClassLoader().getResource(path).getFile();
-				codePath = new URL(path.substring(0, path.lastIndexOf('!')));
-				System.out.printf("Searching JAR: '%s'\n", codePath);
-				// TODO Load resources from inside jar
-				
-			} catch (MalformedURLException e) {
+				// Create a URLClassLoader to load the builtin module
+				URLClassLoader ucl = new URLClassLoader(new URL[] { codePath });
+				Class<Module> builtinModuleClass = (Class<Module>) ucl.loadClass("modules.pixadv.Module");
+				ModuleLoader builtinModule = builtinModuleClass.getConstructor().newInstance();
+				MODULES.add(String.format("%s@%s", builtinModule.getID(), builtinModule.getVersion()));
+				// Load tile textures
+				System.out.println("Loading tiles from <BUILTIN>...");
+				HashMap<String, Class<? extends TileObject>> tiles = builtinModule.loadTiles();
+				for (String id : tiles.keySet()) {
+					String idNew = String.format("%s/%s", builtinModule.getID(), id);
+					TILES.put(idNew, tiles.get(id));
+					System.out.printf("  Registered '%s' (%s)\n", idNew, TILES.get(idNew).getName());
+				}
+				// Close URLClassLoader
+				ucl.close();
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		}
-		try {
-			// Create a URLClassLoader to load the builtin module
-			URLClassLoader ucl = new URLClassLoader(new URL[] { codePath });
-			Class<Module> builtinModuleClass = (Class<Module>) ucl.loadClass("modules.pixadv.Module");
-			ModuleLoader builtinModule = builtinModuleClass.getConstructor().newInstance();
-			MODULES.add(String.format("%s@%s", builtinModule.getID(), builtinModule.getVersion()));
-			// Load tile textures
-			System.out.println("Loading tiles from <BUILTIN>...");
-			HashMap<String, Class<? extends TileObject>> tiles = builtinModule.loadTiles();
-			for (String id : tiles.keySet()) {
-				String idNew = String.format("%s/%s", builtinModule.getID(), id);
-				TILES.put(idNew, tiles.get(id));
-				System.out.printf("  Registered '%s' (%s)\n", idNew, TILES.get(idNew).getName());
-			}
-			// Close URLClassLoader
-			ucl.close();
-		} catch (Exception e) {
+		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
 	}
